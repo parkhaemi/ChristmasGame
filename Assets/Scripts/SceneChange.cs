76ef608dff08b23623b8d6b34fa3663002c51752
parkhaemi <haemipark21@gmail.com>
2020-12-25 01:36:48 +0900

    201225

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+   public void ChangeToSeclctMap()
+    {
+        SceneManager.LoadScene("SelectMap");
+    }
+
+    public void ChangeFirstScene()
+    {
+        SceneManager.LoadScene("Level01");
+    }
+}
