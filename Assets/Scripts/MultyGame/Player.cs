eba0cdc0542c3aea7fadbc670ce0db0d082e63d4
parkhaemi <haemipark21@gmail.com>
2021-02-26 14:40:26 +0900

    21.02.26
    update:mutli-game

---
+++ Assets/Scripts/MultyGame/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : Photon.MonoBehaviour
+{
+    public PhotonView photonView;
+    public Rigidbody2D rb;
+    public GameObject PlayerCamera;
+    public SpriteRenderer sr;
+    public Text PlayerNameText;
+
+    public bool IsGrounded = false;
+    public float MoveSpeed;
+    public float JumpForce;
+
+
+    private void Awake()
+    {
+        if(photonView.isMine)
+        {
+            PlayerCamera.SetActive(true);
+        }
+    }
+
+    private void Update()
+    {
+        if(photonView.isMine)
+        {
+            CheckInput();
+        }
+    }
+
+    private void CheckInput()
+    {
+        var move = new Vector3(Input.GetAxisRaw("Horizontal"), 0);
+        transform.position +=move * MoveSpeed * Time.deltaTime;
+
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            sr.flipX = true;
+        }
+
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            sr.flipX = false;
+        }
+    }
+    
+
+
+
+    
+}
